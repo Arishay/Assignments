36343e29faba7b6cfc3016bcbe8b9d6147534330
AlizaGhaffar <164041342+AlizaGhaffar@users.noreply.github.com>
2024-05-06 09:22:10 -0700

    completed assignment 9 & 10

---
+++ assignment 9 10/app.js
@@ -0,0 +1,6 @@
+"use strict";
+//Assignment number 9
+//Favorite Number: Store your favorite number in a variable.
+// Then, using that variable, create a message that reveals your favorite number. Print that message.
+let FavouriteNumber = 22;
+console.log(`My favourite number is: ${FavouriteNumber}`);
---
+++ assignment 9 10/app.ts
@@ -0,0 +1,30 @@
+//Assignment number 9
+//Favorite Number: Store your favorite number in a variable.
+// Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let FavouriteNumber = 22;
+console.log(`My favourite number is: ${FavouriteNumber}`);
+
+//Assignment #10
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each.//
+// If you don’t have anything specific to write because your programs are too simple at this point, 
+//just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+//program number 1 
+
+let age = 15;
+let message = `my age is ${age}`
+console.log(message);
+
+//name: Arisha Ghaffar
+//Date: 6 of may 2024
+
+// this is my program in hich i have used template literal and then 
+//a variable age with the value 15, then creates another variable message using a template literal.
+// then print this message.
+
+//FOR THE TEACHER :
+// this project is only made by ARISHA Ghaffar.
+//me and my sister both uses the same laptop thats why 
+//whenever we upload our projects on github it shows that this is made by both of us.
+
