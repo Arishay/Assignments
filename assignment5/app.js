1c49b381f21d4a472a2f7d7440dea24df151f987
AlizaGhaffar <164041342+AlizaGhaffar@users.noreply.github.com>
2024-05-06 08:34:34 -0700

    Added asssignment 7

---
+++ assignment5/app.js
@@ -0,0 +1,24 @@
+"use strict";
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8.
+// Be sure to enclose your operations in print statements to see the results
+// ADDITION
+let num1 = 6;
+let num2 = 2;
+let addition = num1 + num2; //8
+console.log(addition);
+;
+//SUBTRACTION
+let number1 = 10;
+let number2 = 2;
+let subtraction = number1 - number2; //8
+console.log(subtraction);
+//MULTIPLICATION
+let value1 = 4;
+let value2 = 2;
+let multiplication = value1 * value2; //8
+console.log(multiplication);
+//DIVISION
+let digit1 = 16;
+let digit2 = 2;
+let division = digit1 / digit2; //8
+console.log(division);
