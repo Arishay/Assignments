60de02f8c757e282f83d33b1a947084d4d821482
AlizaGhaffar <164041342+AlizaGhaffar@users.noreply.github.com>
2024-05-08 10:30:21 -0700

    Completed 11to13 assignment

---
+++ assignment11,12and13/app.js
@@ -0,0 +1,24 @@
+"use strict";
+//ASSIGNMENT# 11
+//Names: Store the names of a few of your friends in a array called names.
+// Print each person’s name by accessing each element in the list, one at a time.
+let friendNames = ['Zara', 'Aina', 'Amna', 'huma'];
+for (let i = 0; i < friendNames.length; i++) {
+    console.log(friendNames[i]);
+}
+//ASSIGNMENT# 12
+//Greetings: Start with the array you used in Exercise 11, 
+//but instead of just printing each person’s name, print a message to them.The text of each 
+//message should be the same,but each message should be personalized with the person’s name.
+let friends = ["Zara", "Aina", "Amna", "Huma"];
+for (let name of friends) {
+    console.log(`Hello! ${name}, Happy friendship day`);
+}
+//ASSIGNMENT# 13
+//Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, 
+//and make a list that stores several examples. Use your list to print a series of statements about these items, 
+//such as “I would like to own a Honda motorcycle.”
+let favouriteTransportation = ["Hilux", "Electrric bike", "Mountain Bicycle"];
+for (let brand of favouriteTransportation) {
+    console.log(`I would like to own a ${brand}, `);
+}
