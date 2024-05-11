e85b3e6b4e825fe06805c74b7fdded71495ee394
AlizaGhaffar <164041342+AlizaGhaffar@users.noreply.github.com>
2024-05-11 08:46:17 -0700

    task23 completed

---
+++ task 23/app.js
@@ -0,0 +1,24 @@
+var number1 = 6;
+var number2 = 7;
+//Testing of Tru2
+console.log("Test1:Number 6 is equal to 6");
+console.log(number1 === 6); //True
+console.log("Test2:Number 6 is Not equal to 9");
+console.log(number1 != 9); //True
+console.log("Test3:Number 7 is equal to 7*1");
+console.log(number2 === 7 * 1); //True
+console.log("Test4:Number 6 is equal to 3+3");
+console.log(number1 === 3 + 3); //True
+console.log("Test5:Number 7 is equal to 4+3");
+console.log(number2 === 4 + 3); //True
+//Testing of False
+console.log("Test6:Number7  is equal to 6");
+console.log(number2 === 6); //False
+console.log("Test7:Number 7 is Not equal to 7");
+console.log(number2 != 7); //False
+console.log("Test8:Number 6 is equal to 2*4 ");
+console.log(number1 === 2 * 4); //False
+console.log("Test9:Number 6 is notequal to 3+3");
+console.log(number1 != 3 + 3); //False
+console.log("Test10:Number 7 is equal to 9-3");
+console.log(number2 === 9 - 3); //False
