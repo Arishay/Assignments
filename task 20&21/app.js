9e33ef4da4fea83af829aae7242b16faf46f85a3
AlizaGhaffar <164041342+AlizaGhaffar@users.noreply.github.com>
2024-05-11 08:08:01 -0700

    task 20 and 21 completed

---
+++ task 20&21/app.js
@@ -0,0 +1,15 @@
+//Assignment 20
+var fruits = ["Apple", "Grapes", "pomegranate", "Mango"];
+console.log("My favourite fruits are:");
+fruits.forEach(function (fruits) { return console.log(fruits); });
+//Assignment 21
+var Student = {
+    name: "Arisha",
+    age: 14,
+    standard: 9,
+};
+console.log("The name of student is ".concat(Student.name, ", her age is ").concat(Student.age, " and she is in class ").concat(Student.standard));
+//FOR THE TEACHER :
+// this project is only made by arisha Ghaffar.
+//me and my sister both uses the same laptop thats why
+//whenever we upload our projects on github it shows that this is made by both of us
---
+++ task 20&21/app.ts
@@ -0,0 +1,27 @@
+//Assignment 20
+
+let fruits: string[] = ["Apple", "Grapes", "pomegranate", "Mango"];
+console.log("My favourite fruits are:");
+
+fruits.forEach(fruits => console.log(fruits));
+
+//Assignment 21
+
+let Student: {
+  name: string;
+  age: number;
+  standard: number;
+} = {
+  name: "Arisha",
+  age: 14,
+  standard: 9,
+};
+console.log(
+  `The name of student is ${Student.name}, her age is ${Student.age} and she is in class ${Student.standard}`
+);
+
+
+//FOR THE TEACHER :
+// this project is only made by arisha Ghaffar.
+//me and my sister both uses the same laptop thats why
+//whenever we upload our projects on github it shows that this is made by both of us
