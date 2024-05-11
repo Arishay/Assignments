c77a5d9add32dc81b78eb54e7a88fd2876b86cd9
AlizaGhaffar <164041342+AlizaGhaffar@users.noreply.github.com>
2024-05-11 07:42:13 -0700

    task 19 completed

---
+++ task19/app.js
@@ -0,0 +1,7 @@
+//picked assignment 14
+var guest = ["Aliza", "Hunny", "Haniya"];
+// guest.forEach(guest => {
+//     console.log(`${guest}, will you come to join me for dinner?`);
+// });
+var numberOfGuests = guest.length;
+console.log("Total number of guests are ".concat(numberOfGuests));
---
+++ task19/app.ts
@@ -0,0 +1,16 @@
+//Task 19
+
+//I have picked assignment 14
+let guest:string[] = ["Aliza" , "Hunny" , "Haniya"];
+
+// guest.forEach(guest => {
+//     console.log(`${guest}, will you come to join me for dinner?`);
+// });
+
+let numberOfGuests = guest.length;
+console.log(`Total number of guests are ${numberOfGuests}`);
+
+//FOR THE TEACHER :
+// this project is only made by arisha Ghaffar.
+//me and my sister both uses the same laptop thats why 
+//whenever we upload our projects on github it shows that this is made by both of us
