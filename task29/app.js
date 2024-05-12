fad235eb7ab4e4c47a4d124ced990b519ede2103
Arisha Ghaffar <160336030+Arishay@users.noreply.github.com>
2024-05-11 23:42:51 -0700

    completed task29

---
+++ task29/app.js
@@ -0,0 +1,16 @@
+var favouriteFriut = ["Apple", "Mango", "Cherry"];
+if (favouriteFriut.includes("Mango")) {
+    console.log("I really like Mango");
+}
+if (favouriteFriut.includes("Cherry")) {
+    console.log("I really like Cherry");
+}
+if (favouriteFriut.includes("Apple")) {
+    console.log("I really like apple");
+}
+if (favouriteFriut.includes("Banana")) {
+    console.log("I really like Banana");
+}
+if (favouriteFriut.includes("Kiwi")) {
+    console.log("I really like Kiwi");
+}
---
+++ task29/app.ts
@@ -0,0 +1,17 @@
+let favouriteFriut:string[] = ["Apple", "Mango", "Cherry"];
+
+if (favouriteFriut.includes("Mango")) {
+    console.log("I really like Mango");  
+}
+if (favouriteFriut.includes("Cherry")) {
+    console.log("I really like Cherry");  
+}
+if (favouriteFriut.includes("Apple")) {
+    console.log("I really like apple");  
+}
+if (favouriteFriut.includes("Banana")) {
+    console.log("I really like Banana");  
+}
+if (favouriteFriut.includes("Kiwi")) {
+    console.log("I really like Kiwi");  
+}
