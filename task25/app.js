f9cc0d896fd9d756af9837964c3e283c833ee11e
Arisha Ghaffar <160336030+Arishay@users.noreply.github.com>
2024-05-11 21:56:50 -0700

    task 25 completed

---
+++ task25/app.js
@@ -0,0 +1,6 @@
+var alien_color = "green";
+if (alien_color === "green") {
+    console.log("You just earned 5 points!");
+}
+var alien_color2 = "yellow";
+if (alien_color == "red") { }
---
+++ task25/app.ts
@@ -0,0 +1,15 @@
+let alien_color = "green";
+if (alien_color === "green") {
+    console.log("You just earned 5 points!");
+}
+
+let alien_color2 = "yellow";
+if (alien_color == "red") {}
+
+
+//FOR THE TEACHER :
+//This project is only made by arisha Ghaffar.
+//me and my sister both uses the same laptop thats why
+//whenever we upload our projects on github it shows that this is made by both of us.
+
+
