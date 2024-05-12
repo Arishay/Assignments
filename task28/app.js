4175df68dad8bcda4bdd39d36af5043e34176fe9
Arisha Ghaffar <160336030+Arishay@users.noreply.github.com>
2024-05-11 23:31:06 -0700

    completed task 28

---
+++ task28/app.js
@@ -0,0 +1,16 @@
+var age = 67;
+if (age < 2) {
+    console.log("you are a baby.");
+}
+else if (age >= 2 && age < 4) {
+    console.log("you are a toddler");
+}
+else if (age >= 4 && age < 13) {
+    console.log("you are a Kid");
+}
+else if (age >= 20 && age < 65) {
+    console.log("you are a adult");
+}
+else if (age >= 65) {
+    console.log("you are a elder");
+}
