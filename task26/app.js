343b7f4bfb55fc8458cad7e1c7fdb1a63f10b203
Arisha Ghaffar <160336030+Arishay@users.noreply.github.com>
2024-05-11 22:53:04 -0700

    completed task26

---
+++ task26/app.js
@@ -0,0 +1,16 @@
+//First Version
+var alienColor = "green";
+if (alienColor === "green") {
+    console.log("You just earned 5 points for shooting the alien!");
+}
+else {
+    console.log("You just earned 10 points.");
+}
+//Second Version
+var alienColor2 = "yellow";
+if (alienColor2 === "green") {
+    console.log("You just earned 5 points for shooting the alien!");
+}
+else {
+    console.log("You just earned 10 points.");
+}
---
+++ task26/app.ts
@@ -0,0 +1,17 @@
+//First Version
+
+let alienColor = "green";
+if (alienColor === "green") {
+    console.log("You just earned 5 points for shooting the alien!");
+} else {
+    console.log("You just earned 10 points.");
+}
+
+//Second Version
+
+let alienColor2 = "yellow";
+if (alienColor2 === "green") {
+    console.log("You just earned 5 points for shooting the alien!");
+} else {
+    console.log("You just earned 10 points.");
+}
